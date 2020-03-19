ff33ceb629d3bdf5cf88f752679870ef5aa5267f
roxtigger2k3 <rfechinonguyen@gmail.com>
2020-03-19 10:26:54 +0000

    gg

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const __GLOBAL = new Object({
     swearList: new Array(),
     confirm: new Array(),
 });
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 facebook = ({ Op, models }) => login({ email, password, appState: require(appStateFile) }, function (error, api) {
     if (error) return logger(error, 2);
     fs.writeFileSync(appStateFile, JSON.stringify(api.getAppState(), null, '\t'));
